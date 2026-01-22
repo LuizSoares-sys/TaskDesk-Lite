8215eafb957d0efbbb4c69d393788553fddd0669
claudio.gbgomes <claudio.gbgomes@TIT0676438W11-1>
2026-01-22 16:43:50 -0300

    add de teste de palavra proibida

--- TaskDeskLite-master/TaskDeskLite.Tests/UnitTest1.cs
+++ TaskDeskLite-master/TaskDeskLite.Tests/UnitTest1.cs
@@ -5,12 +5,12 @@ namespace TaskDeskLite.Tests
     public class UnitTest1
     {
         // TESTE PARA VERIFICAR SE A DESCRIÇÃO NÃO ULTRAPASSA 200 CARACTERES
-        [Fact(DisplayName = "Descricao se limita a 201 caracters")]
+        [Fact(DisplayName = "Descricao se limita a 200 caracters")]
         public void Descricao200Caracters()
         {
 
             // Cria uma string com 201 caracteres para ultrapassar o limite permitido (200)
-            var DescricaoInvalida = new string('a', 201); // 201 caracteres 'a'
+            var DescricaoInvalida = new string('a', 201); // 201 caracteres da letra 'a'
 
             // Cria uma tarefa válida em todos os aspectos,
             // exceto pela descrição que é maior que 200 caracteres
@@ -30,6 +30,26 @@ namespace TaskDeskLite.Tests
 
             // Verifica se a mensagem da exceção é exatamente a esperada
             Assert.Equal("Descrição deve ter no máximo 200 caracteres.", exception.Message);
+        }
+
+        // TESTE PARA VERIFICAR SE O TÍTULO NÃO CONTÉM PALAVRAS PROIBIDAS
+        [Fact(DisplayName = "Palavras Bloqueadas")]
+        public void PalavrasBloqueadas()
+        {
+            // Cria uma tarefa com uma palavra proibida no título
+            var task = new TaskItem
+            {
+                Title = "DROP TABLE tasks", // Palavra proibida "hack"
+                Description = "Descrição válida",
+                Priority = TaskPriority.Medium
+            };
+            // Executa o método de validação e verifica se ele lança
+            // uma DomainValidationException
+            var exception = Assert.Throws<DomainValidationException>(() =>
+                TaskValidator.ValidateForCreateOrUpdate(task)
+            );
+            // Verifica se a mensagem da exceção é exatamente a esperada
+            Assert.Equal("Título contém termo não permitido.", exception.Message);
         }
     }
 }
