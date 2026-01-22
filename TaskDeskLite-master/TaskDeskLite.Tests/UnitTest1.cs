b4ef907152bc645f1e86fad17cf27d6761dd0e49
LUIZ FELIPE MATHEUS DA SILVA <luiz.fmsilva23@senacsp.edu.br>
2026-01-22 16:58:47 -0300

    Test de Prazo

--- TaskDeskLite-master/TaskDeskLite.Tests/UnitTest1.cs
+++ TaskDeskLite-master/TaskDeskLite.Tests/UnitTest1.cs
@@ -1,11 +1,22 @@
-﻿namespace TaskDeskLite.Tests
+﻿using TaskDeskLite.Core;
+
+namespace TaskDeskLite.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Fact(DisplayName = "Prazo da data do cadastro")]
+        public void Create_Deve_Definir_CreatedAt_Com_Data_De_Hoje()
         {
+            var service = new TaskService();
 
+            var task = service.Create(new TaskItem
+            {
+                Title = "Teste",
+                Priority = TaskPriority.Medium
+            });
+
+            Assert.Equal(DateTime.Today, task.CreatedAt.Date);
         }
+
     }
 }
